64742eeb64fcc56bb790f03a230ff94c4dfba472
Paulo Santos <124013397+PauloSantos223@users.noreply.github.com>
2024-05-08 00:02:06 -0400

    Finished Making Kaboom Portfolio

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my computer I built myself. I use it mainly for school and to ocasionally 
+        play games with my friends. Here is my <a href="https://github.com/PauloSantos223" target="_blank">Github</a>!`,
+    "cs-degree": `This is my soon to be UI/UX Certificate.`,
+    "sofa-table": `This is where I relax and watch shows or Youtube.`,
+    tv: `This is my TV. I watch whatever I'm in the mood for and that can be games, shows, to even
+        watching Startalk with Neil deGrasse Tyson.`,
+    bed: `This is where I go to sleep. I love to sometimes just relax or
+        watch videos that make you have thoughts that leave you scracthing your head.`,
+    resume: `This is my soon to be resume. You can contact me at this email: zebroa10@gmail.com`,
+    projects: `This project was made using the Kaboom library.
+        The text you are currently reading is being rendered with HTML/CSS.`,
+    library: `This is where I keep some of my textbooks and other appliances.`,
+    exit: `Do you already want to leave? Fine, just close close the tab.`,
+        
+};
